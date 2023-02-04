132d12789d4b06c255c7894ddf7fac9c0d5ccba6
mx0c <marius-bauer-2006@web.de>
2023-02-04 14:30:11 +0100

    init commit

---
+++ main.py
@@ -0,0 +1,80 @@
+from sys import stdout
+from time import sleep
+from steamy import SteamAPI, SteamMarketAPI
+import datetime
+import os
+
+CONDITIONS = ["Battle-Scarred", "Well-Worn", "Field-Tested", "Minimal Wear", "Factory New"]
+TIME_INTERVAL = 15
+
+
+class SteamPriceScraper:
+    def __init__(self):
+        steam = SteamAPI("API_KEY")
+        self.steam_market = steam.market(730)
+
+    # Can be used to retrieve all csgo items from the steam api. Saves them into an csv file.
+    def extract_all_itemnames(self):
+        current_progress = 0
+        try:
+            with open("allitems.csv", "r", encoding="utf-8") as f:
+                read_items = f.readlines()
+                current_progress = len(read_items)
+        except FileNotFoundError:
+            print("no progress made scraping all itemnames. Starting now!")
+
+        item_amount = self.steam_market.get_item_count()
+
+        for i in range(current_progress,item_amount,100):
+            item = self.steam_market.list_items("", i, 100, "quantity", "desc")
+            item_list = list(item)
+            
+            with open("allitems.csv", "a", encoding="utf-8") as f:
+                f.write("\n" + "\n".join(item_list))
+            
+            for idx in range (0,100):
+                percent = (i+idx) / item_amount * 100 
+                stdout.write(f"\rProgress {i+idx} of {item_amount} => {percent:.2f}%")
+                stdout.flush()
+                sleep(TIME_INTERVAL/100)
+
+    # loads all itemnames from allitems.csv
+    def load_all_itemnames(self):
+        with open("allitems.csv", "r", encoding="utf-8") as f:
+            items = []
+            for line in f.readlines():
+                items.append(line.rstrip())
+        return items
+
+    # calculates the difference of the price between the current date and date(time_delta_in_days)
+    def calculate_price_diff(self, item_name, time_delta_in_days):
+        test = self.steam_market.get_item_price_history(item_name)
+        filtered = {k: v for k, v in test.items() if (datetime.datetime.now() - datetime.timedelta(days=time_delta_in_days)) < k < datetime.datetime.now()}
+        prices = list(filtered.values())
+
+        diff = prices[len(prices)-1] - prices[0]
+        diff_in_perc = round(diff / prices[0] * 100, 2) 
+
+        return f"{diff_in_perc} %"
+
+
+if __name__ == '__main__':
+    steam_price_scraper = SteamPriceScraper()
+    #steamPriceScraper.extract_all_itemnames()
+    all_items = steam_price_scraper.load_all_itemnames()
+
+    current_progress = 0
+    try:
+        with open("result.csv", "r", encoding="utf-8") as f:
+            current_progress = len(f.readlines())
+    except FileNotFoundError:
+        print("no progress made calculating pricetrends. Starting now!")
+
+    for idx in range(current_progress, len(all_items)):
+        item = all_items[idx]
+        price_diff = steam_price_scraper.calculate_price_diff(item, 30)
+        with open("result.csv", "a", encoding="utf-8") as f:
+            f.write(f"{item} : {price_diff}\n")
+            stdout.write(f"\rProgress: {idx+1} of {len(all_items)}")
+            stdout.flush()
+        sleep(TIME_INTERVAL)
